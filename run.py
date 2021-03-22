79cad22cc14ea566fce8498d0b8db92420d44c6d
Rohan Sikand <57341225+rosikand@users.noreply.github.com>
2021-03-22 17:19:55 -0400

    Update run.py

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ is then sent to the training model. The output is printed out.
 
 from pred import predict_text
 
-print('This program classifies a a sequence of text as either "partisan" or "neutral". Enter some text below and see the classification!')
+print('This program classifies a sequence of text as either "partisan" or "neutral". Enter some text below and see the classification!')
 
 # get user input 
 txt = input('Enter text: ')
